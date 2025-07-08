c079fa368bd483bde2db1fa9e1686485a4ad3723
김우성 <KimHyangNam09@gmail.com>
2025-07-08 20:55:29 +0900

    [StartSettingsController] LoginPanel 기능 추가

--- Assets/02_Script/01_UI/02_Settings/StartSettingsController.cs
+++ Assets/02_Script/01_UI/02_Settings/StartSettingsController.cs
@@ -1,9 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartSettingsController : MonoBehaviour, IUIPanel
 {
-    public void Show() => gameObject.SetActive(true);
+    [SerializeField] private Button newGameButton;
+    [SerializeField] private UIManager uiManager;
 
+    private void Awake()
+    {
+        newGameButton.onClick.AddListener(OnNewGameClicked);
+    }
+
+    private void OnDestroy()
+    {
+        newGameButton.onClick.RemoveListener(OnNewGameClicked);
+    }
+
+    public void Show() => gameObject.SetActive(true);
     public void Hide() => gameObject.SetActive(false);
-    
+
+    private void OnNewGameClicked()
+    {
+        uiManager.ShowPanel<LoginPanel>(); // 원하는 패널로 전환
+    }
 }
