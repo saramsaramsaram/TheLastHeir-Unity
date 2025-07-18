1f863cf8512de42a7542a9af49e55471cdcd64c2
김우성 <KimHyangNam09@gmail.com>
2025-07-18 13:57:26 +0900

    fix : Server측에서 토큰 발급 안됨, 문제 확인

--- Assets/02_Script/01_UI/05_Manager/LoginManager.cs
+++ Assets/02_Script/01_UI/05_Manager/LoginManager.cs
@@ -11,18 +11,31 @@ public class LoginManager : MonoBehaviour
     public TMP_InputField passwordInput;
     public string nextSceneName;
 
-    private const string loginUrl = "https://831e612033c4.ngrok-free.app/api/accounts";
+    private const string loginUrl = "https://831e612033c4.ngrok-free.app/api/accounts/login";
 
     public void OnLoginButtonClicked()
     {
+        TMP_InputField usernameInput = GameObject.Find("InputID").GetComponent<TMP_InputField>();
+        TMP_InputField passwordInput = GameObject.Find("InputPW").GetComponent<TMP_InputField>();
+
+        if (usernameInput == null || passwordInput == null)
+        {
+            Debug.LogError("InputField를 찾을 수 없음");
+            return;
+        }
+
         string username = usernameInput.text;
         string password = passwordInput.text;
+        
+        Debug.Log($"로그인 정보: {username} / {password}");
 
         StartCoroutine(TryLogin(username, password));
     }
 
     private IEnumerator TryLogin(string username, string password)
     {
+        Debug.Log("API 요청 시작");
+        Debug.Log("로그인 시도" + username + " / " + password);
         LoginRequest requestData = new LoginRequest
         {
             username = username,
@@ -36,12 +49,14 @@ public class LoginManager : MonoBehaviour
         request.uploadHandler = new UploadHandlerRaw(jsonBytes);
         request.downloadHandler = new DownloadHandlerBuffer();
         request.SetRequestHeader("Content-Type", "application/json");
-
+        
         yield return request.SendWebRequest();
+        Debug.Log("API 요청 끝, 응답: " + request.downloadHandler.text);
 
         if (request.result == UnityWebRequest.Result.Success && request.responseCode == 200)
         {
             string responseJson = request.downloadHandler.text;
+            Debug.Log("API 응답: " + responseJson);
             TokenResponse token = JsonUtility.FromJson<TokenResponse>(responseJson);
 
             PlayerPrefs.SetString("jwt_token", token.token);
@@ -52,7 +67,7 @@ public class LoginManager : MonoBehaviour
         }
         else
         {
-            Debug.LogWarning($"로그인 실패: {request.responseCode} - {request.downloadHandler.text}");
+            Debug.LogWarning($"로그인 실패: {request.responseCode} - {request.downloadHandler.text} / {request.error}");
         }
     }
 }
