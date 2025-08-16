b8614f4e8e8ba7122689bf683a8f4a173d671bde
saramsaramsaram <leejuhwan00000@gmail.com>
2025-08-16 20:59:35 +0900

    Delete Assets/03_Player/DialogueData.cs

--- Assets/03_Player/DialogueData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue/DialogueData")]
-public class DialogueData : ScriptableObject
-{
-    [TextArea(2, 5)]
-    public string[] sentences;
-}
